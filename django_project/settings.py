4e91b38d18d6dbb9325783893ba3d512d929a6aa
Samson OCHE <s.oche@outlook.com>
2026-02-13 10:45:37 +0100

    Postgree DB

--- django_project/settings.py
+++ django_project/settings.py
@@ -114,14 +114,14 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'railway',
-        # 'USER': 'postgres',
-        # 'PASSWORD': os.environ['DB_PASSWORD_PG'],
-        # 'HOST': 'crossover.proxy.rlwy.net',
-        # 'PORT': '11067',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.environ['DB_PASSWORD_PG'],
+        'HOST': 'crossover.proxy.rlwy.net',
+        'PORT': '11067',
     }
 }
 
